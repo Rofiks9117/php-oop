d6e267b07f5375dede7f34ed4f5b693a94774391
Nikita Khilko <xilko03@bk.ru>
2022-11-24 19:03:19 +0700

    58

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php 
-require_once 'date.php';
-require_once 'interval.php';
+require_once 'iFile.php';
+require_once 'file.php';
 
-	$date1 = new Date('2025-12-31');
-	$date2 = new Date('2026-11-28');
+	$file = new File('index.php');
 
-	$interval = new Interval($date1, $date2);
+echo $file->getPath().'<br>';
+echo $file->getDir().'<br>';
+echo $file->getExt().'<br>';
+echo $file->getName().'<br>';
+echo $file->getSize().' байт'.'<br>';
 
-	echo $interval->toDays();   //  выведет разницу в днях
-	echo '<br>';
-	echo $interval->toMonths(); //  выведет разницу в месяцах
-	echo '<br>';
-	echo $interval->toYears();  //  выведет разницу в годах
+echo $file->getText();
+
 ?>
