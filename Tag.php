c37e91f88c2b59a9a1187bc0129b06a4d45c2721
Nikita Khilko <xilko03@bk.ru>
2022-11-24 21:21:48 +0700

    65

--- Tag.php
+++ Tag.php
@@ -24,7 +24,7 @@
 	{
 		if (!empty($attrs)) {
 			$result = '';
-
+			
 			foreach ($attrs as $name => $value) {
 				if ($value === true) {
 					$result .= " $name"; 
@@ -32,7 +32,7 @@
 					$result .= " $name=\"$value\""; 
 				}
 			}
-
+			
 			return $result;
 		} else {
 			return '';
@@ -56,6 +56,41 @@
 			foreach ($attrs as $name => $value) {
 			$this->setAttr($name, $value);
 		}
+		return $this;
+	} 
+	public function addClass($className)
+	{
+		if (isset($this->attrs['class'])) {
+			$classNames = explode(' ', $this->attrs['class']);
+
+			if (!in_array($className, $classNames)) {
+				$classNames[] = $className;
+				$this->attrs['class'] = implode(' ', $classNames);
+			}
+		} else {
+			$this->attrs['class'] = $className;
+		}
+
+		return $this;
+	}
+	private function removeElem($elem, $arr)
+	{
+		$key = array_search($elem, $arr); // находим ключ элемента по его тексту
+		array_splice($arr, $key, 1); // удаляем элемент
+
+		return $arr; // возвращаем измененный массив
+	}
+	public function removeClass($className)
+	{
+		if (isset($this->attrs['class'])) {
+			$classNames = explode(' ', $this->attrs['class']);
+
+			if (in_array($className, $classNames)) {
+				$classNames = $this->removeElem($className, $classNames);
+				$this->attrs['class'] = implode(' ', $classNames);
+			}
+		}
+
 		return $this;
 	}
 } 
