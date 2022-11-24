e24d30bb7ac5f13b7b3f87cd057ddc89a1e9f605
Nikita Khilko <xilko03@bk.ru>
2022-11-24 22:13:38 +0700

    67

--- Tag.php
+++ Tag.php
@@ -1,9 +1,10 @@
 <?php
-	class Tag
+	class Tag implements iTag
 	{
 		private $name;
 		private $attrs = [];
-		
+		private $text = '';
+
 		public function __construct($name)
 		{
 			$this->name = $name;
@@ -12,14 +13,22 @@
 		public function open()
 		{
 			$name = $this->name;
-			return "<$name>";
+			$attrsStr = $this->getAttrsStr($this->attrs);
+
+			return "<$name$attrsStr>";
 		}
-		
+
 		public function close()
 		{
 			$name = $this->name;
 			return "</$name>";
 		}
+
+		public function show()
+		{
+			return $this->open() . $this->text . $this->close();
+		}
+
 		private function getAttrsStr($attrs)
 	{
 		if (!empty($attrs)) {
@@ -37,20 +46,26 @@
 		} else {
 			return '';
 		}
-	} 
-	public function setAttr($name, $value)
+	}
+
+	public function setText($text)
+		{
+			$this->text = $text;
+			return $this;
+		}
+
+	public function setAttr($name, $value = true)
 		{
 			$this->attrs[$name] = $value;
-			return $this; 
+			return $this;
 		} 
+
 	public function removeAttr($name)
 		{
-			if(array_key_exists($name, $this->attrs))
-			{
 			unset($this->attrs[$name]);
-			}
-		return $this;
+			return $this;
 		}
+
 	public function setAttrs($attrs)
 		{
 			foreach ($attrs as $name => $value) {
@@ -58,6 +73,7 @@
 		}
 		return $this;
 	} 
+
 	public function addClass($className)
 	{
 		if (isset($this->attrs['class'])) {
@@ -70,16 +86,18 @@
 		} else {
 			$this->attrs['class'] = $className;
 		}
-		
+
 		return $this;
 	}
+
 	private function removeElem($elem, $arr)
 	{
 		$key = array_search($elem, $arr); // находим ключ элемента по его тексту
 		array_splice($arr, $key, 1); // удаляем элемент
-		
+
 		return $arr; // возвращаем измененный массив
 	}
+
 	public function removeClass($className)
 	{
 		if (isset($this->attrs['class'])) {
@@ -90,25 +108,33 @@
 				$this->attrs['class'] = implode(' ', $classNames);
 			}
 		}
-
+		
 		return $this;
 	}
-
+	
 	public function getName()
 	{
 		return $this->name;
 	}
+
 	public function getText()
 	{
 		return $this->text;
 	}
+
 	public function getAttrs()
 	{
 		return $this->attrs;
 	}
-	public function getAttr($attr)
-	{
-		return $this->attrs[$attr];
-	}
+
+	public function getAttr($name)
+		{
+			if (isset($this->attrs[$name])) {
+				return $this->attrs[$name];
+			} else {
+				return null;
+			}
+		}
+
 } 
 ?>
