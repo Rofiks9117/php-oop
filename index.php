a56104d1abb8e55b10580736dd7f659196c4a794
Nikita Khilko <xilko03@bk.ru>
2022-11-25 01:05:08 +0700

    70.2-3

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
 	require_once 'HtmlList.php';
-	$list = new HtmlList('ul');
+	require_once 'ClassUL.php';
+	require_once 'ClassOl.php';
+	$ul = new Ul; 
+	$ol = new Ol; 
 
-	echo $list
+	echo $ol
 		->addItem((new ListItem())->setText('item1'))
 		->addItem((new ListItem())->setText('item2'))
 		->addItem((new ListItem())->setText('item3'))
-		->show();
+		->show(); 
+
+	echo '<br>';
+	echo $ul
+		->addItem((new ListItem())->setText('item1'))
+		->addItem((new ListItem())->setText('item2'))
+		->addItem((new ListItem())->setText('item3'))
+		->show(); 
+
 ?>
